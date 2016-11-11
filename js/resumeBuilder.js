8074185b557f1f3ce2ad2dfd1951fc90ea3ad35f
Julia Kudinovich <julia.kudinovich@gmail.com>
2016-11-11 15:04:11 -0800

    work in progress

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,38 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var formattedName = HTMLheaderName.replace("%data%", "Julia");
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+
+
+var skills= ["awesomness", "programming"];
+
+var bio = {
+    "name" : "Julia",
+    "role" : "Web Developer",
+    "contacts" : {
+        "email" : "julia.kudinovich@gmail.com",
+        "mobile" : "123-456-7890",
+        "github" : "julia-kudinovich",
+        "location" : "Las Vegas"
+    },
+    "picture URL" : "i,ages/197x148.jpg",
+    "welcome message" : "Nice to meet you!",
+    "skills" : skills
+};
+
+
+var  work = {};
+work.position = "BI developer";
+work.employer = "AH4R";
+work.years = "2.5";
+work.city = "Las Vegas, NV";
+
+var eduction = {};
+education["name"] = "UCLA";
+education["years"] = "2012-2014";
+education["city"] = "Los Angeles, CA";
+
+$("#main").append(work["position"]);
+$("#main").append(education.name);
