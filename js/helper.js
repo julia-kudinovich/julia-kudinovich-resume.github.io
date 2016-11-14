3af85982f4bc36418df71b52198928555f169a10
Julia Kudinovich <julia.kudinovich@gmail.com>
2016-11-14 14:11:50 -0800

    add all the display sections

--- js/helper.js
+++ js/helper.js
@@ -1,17 +1,3 @@
-/*
-
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
-*/
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr>';
 
@@ -26,7 +12,7 @@ var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</sp
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
@@ -43,14 +29,14 @@ var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' -- %data%</a>';
+var HTMLschoolName = '<a href="#" target="_blank">%data%';
+var HTMLschoolDegree = ' - %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
+var HTMLonlineTitle = '<a href="#" target="_blank">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
@@ -86,7 +72,9 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+    var x = loc.pageX;
+    var y = loc.pageY;
+    logClicks(x, y);
 });
 
 
@@ -239,7 +227,7 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,11 +1,5 @@
-// var formattedName = HTMLheaderName.replace("%data%", "Julia");
-// var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
-
-// $("#header").prepend(formattedRole);
-// $("#header").prepend(formattedName);
-
 var bio = {
-    "name" : "Julia",
+    "name" : "Julia Kudinovich",
     "role" : "Web Developer",
     "contacts" : {
         "email" : "julia.kudinovich@gmail.com",
@@ -19,71 +13,158 @@ var bio = {
     "skills" : ["html", "css", "bootstrap", "php", "sql"]
 };
 
-
 var work = {
-    "jobs" : [
-        {
-            "employer" : "American Homes 4 Rent" ,
-            "title" : "BI Developer",
-            "location" : "Las Vegas, NV",
-            "dates" : "October 2015 - Current",
-            "description" : ""
-        },
-        {
-            "employer" : "American Homes 4 Rent",
-            "title" : "Pricing and Analytics Staff",
-            "location" : "Las Vegas, NV",
-            "dates" : "July 2014 - October 2015",
-            "description" : ""
-        }
-    ]
+    "jobs" : [{
+        "employer" : "American Homes 4 Rent" ,
+        "title" : "BI Developer",
+        "location" : "Las Vegas, NV",
+        "dates" : "October 2015 - Current",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },{
+        "employer" : "American Homes 4 Rent",
+        "title" : "Pricing and Analytics Staff",
+        "location" : "Las Vegas, NV",
+        "dates" : "July 2014 - October 2015",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    }]
 };
 
 var projects = {
-    "projects" : [
-        {
-            "title" : "",
-            "dates" : "",
-            "description" : "",
-            "images" : ["", ""]
-        },
-        {
-             "title" : "",
-            "dates" : "",
-            "description" : "",
-            "images" : ["", ""]
-        }
-    ]
+    "projects" : [{
+        "title" : "Sample Project 1",
+        "dates" : "2016",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "images" : ["http://placehold.it/350x150", "http://placehold.it/350x150"]
+    },{
+        "title" : "Sample Project 2",
+        "dates" : "2017",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "images" : ["http://placehold.it/350x150", "http://placehold.it/350x150"]
+    }]
 };
 
-var eduction = {
-    "schools" : [
-        {
-            "name" : "UCLA",
-            "location" : "Los Angeles",
-            "degree" : "Bachelor of Science",
-            "majors" : ["Math/Applied Science"],
-            "dates" : "2014",
-            "url" : ""
-        },
-        {
-            "name" : "LAVC",
-            "location" : "Los Angeles",
-            "degree" : "Transfer",
-            "majors" : ["Mathematics"],
-            "dates" : "2012",
-            "url" : ""
+var education = {
+    "schools" : [{
+        "name" : "UCLA",
+        "location" : "Los Angeles",
+        "degree" : "Bachelor of Science",
+        "majors" : ["Math/Applied Science"],
+        "dates" : "2014",
+        "url" : "http://www.ucla.edu/"
+    },{
+        "name" : "LAVC",
+        "location" : "Los Angeles",
+        "degree" : "Transfer",
+        "majors" : ["Mathematics"],
+        "dates" : "2012",
+        "url" : "http://www.lavc.edu/"
+        }],
+    "onlineDegrees" : [{
+        "school" : "UDACITY",
+        "title" : "Front-End Web Developer Nanodegree",
+        "dates" : "2017",
+        "url" : "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001?v=fe1"
+    }]
+};
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+if (bio.skills.length > 0 ) {
+    $("#header").append(HTMLskillsStart);
+
+    for (i=0; i < bio.skills.length; i++) {
+        var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+        $("#skills"). append(formattedSkill);
+    }
+}
+
+education.display = function() {
+    for (i=0; i < education.schools.length; i++) {
+        $("#education").append(HTMLschoolStart);
+
+        var formattedName = HTMLschoolName.replace("%data%", education.schools[i].name).replace("#", education.schools[i].url);
+        var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
+        var formattedNameDegree = formattedName + formattedDegree;
+        var formattedDates = HTMLschoolDates.replace("%data%", education.schools[i].dates);
+        var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);
+
+        $(".education-entry:last").append(formattedNameDegree);
+        $(".education-entry:last").append(formattedDates);
+        $(".education-entry:last").append(formattedLocation);
+
+        if (education.schools[i].majors.length > 0 ){
+            for (n=0; n < education.schools[i].majors.length ; n++) {
+                 var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[i].majors[n]);
+                $(".education-entry:last").append(formattedMajor);
+            }
         }
-    ],
-    "onlineDegrees" : [
-          {
-            "school" : "UDACITY",
-            "title" : "Front-End Web Developer Nanodegree",
-            "dates" : "2017",
-            "url" : "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001?v=fe1"
+    }
+
+    if (education.onlineDegrees.length > 0) {
+        $("#education").append(HTMLonlineClasses);
+        for (i=0; i < education.onlineDegrees.length; i++) {
+            $("#education").append(HTMLschoolStart);
+
+            var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineDegrees[i].title).replace("#", education.onlineDegrees[i].url);
+            var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineDegrees[i].school);
+            var formattedTitleSchool = formattedTitle + formattedSchool;
+            var formattedDates = HTMLonlineDates.replace("%data%", education.onlineDegrees[i].dates);
+
+            $(".education-entry:last").append(formattedTitleSchool);
+            $(".education-entry:last").append(formattedDates);
         }
-    ]
+    }
+}
+
+
+
+work.display = function() {
+    for (i=0; i < work.jobs.length; i++) {
+        $("#workExperience").append(HTMLworkStart);
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[i].title);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[i].dates);
+        var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[i].location);
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[i].description);
+        $(".work-entry:last").append(formattedEmployerTitle);
+        $(".work-entry:last").append(formattedDates);
+        $(".work-entry:last").append(formattedLocation);
+        $(".work-entry:last").append(formattedDescription);
+    }
+}
+
+
+projects.display = function() {
+    for (i=0; i < projects.projects.length; i++) {
+        $("#projects").append(HTMLprojectStart);
+
+        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title);
+        $(".project-entry:last").append(formattedTitle);
+
+         var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
+        $(".project-entry:last").append(formattedDates);
+
+         var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
+        $(".project-entry:last").append(formattedDescription);
+
+        if (projects.projects[i].images.length > 0 ){
+            for (n=0; n < projects.projects[i].images.length; n++) {
+                var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[i].images[n]);
+                $(".project-entry:last").append(formattedImage);
+            }
+        }
+
+    }
 };
 
 
+work.display();
+projects.display();
+education.display();
 
+$("#mapDiv").append(googleMap);
